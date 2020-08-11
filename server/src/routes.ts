bd5ba3903735bbd67a7fb568d5ada59ae3d84765
Luís Antônio <codenads@outlook.com>
2020-08-11 16:08:25 -0300

    Backend da aplicação feito

---
+++ server/src/routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+const routes = Router();
+
+routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+routes.post('/points', pointsController.create);
+
+export default routes;
